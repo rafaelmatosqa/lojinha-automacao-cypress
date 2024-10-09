f833fdc390c5a2378dd40490a8393dc8578ec9d1
Rafael Matos <rafaelmatosqa@gmail.com>
2024-10-08 22:10:05 -0300

    ajuste na url base do ci

--- cypress.config.ci.js
+++ cypress.config.ci.js
@@ -1,7 +1,9 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+    baseUrl: "http://165.227.93.41",
   env: {
+    
     USERNAME: process.env.USERNAME,
     PASSWORD: process.env.PASSWORD,
   },
